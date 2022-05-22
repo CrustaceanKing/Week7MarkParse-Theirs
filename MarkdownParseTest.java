2ecf0c7ab2831c0863745fa419d2d26ad15f7f8b
CrustaceanKing <swald@ucsd.edu>
2022-05-22 12:19:07 -0700

    Added Tests for Snippets and the Snippets themsevleves

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,47 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 public class MarkdownParseTest {
 
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void testSnippet1() throws IOException{
+        ArrayList<String> compareString = new ArrayList<String>();
+        compareString.add("'google.com");
+        compareString.add("google.com");
+        compareString.add("ucsd.edu");
+        Path fileName = Path.of("Snippet-1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(compareString, links);
+    }
+
+    @Test
+    public void testSnippet2() throws IOException{
+        ArrayList<String> compareString = new ArrayList<String>();
+        compareString.add("a.com");
+        compareString.add("a.com(())");
+        compareString.add("example.com");
+        Path fileName = Path.of("Snippet-2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(compareString, links);
+    }
+
+    @Test
+    public void testSnippet3() throws IOException{
+        ArrayList<String> compareString = new ArrayList<String>();
+        compareString.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        Path fileName = Path.of("Snippet-3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(compareString, links);
     }
 
 }
